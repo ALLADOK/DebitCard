b780aceb2d98e512e281a57802b2caa4de4d7418
ALLA <all.tarasowa808@yandex.ru>
2024-07-24 22:55:30 +0300

    Reformat test1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 
+[![Java CI with Gradle](https://github.com/ALLADOK/DebitCard/actions/workflows/gradle.yml/badge.svg)](https://github.com/ALLADOK/DebitCard/actions/workflows/gradle.yml)
--- src/test/java/ru/netology/selenide/DebitCardTest.java
+++ src/test/java/ru/netology/selenide/DebitCardTest.java
@@ -1,9 +1,11 @@
 package ru.netology.selenide;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -13,11 +15,12 @@ public class DebitCardTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-        SelenideElement form = $("['theme_alfa-on-white']");
-        form.$("[data-test-id='name'] input").setValue("Иван");
-        form.$("[data-test-id='phone'] input").setValue("+79650000000");
-        form.$("[data-test-id='agreement']").click();
-        form.$("[type='button']").click();
-        $(".order-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время"));
+        $("[data-test-id='name'] input").setValue("Иван");
+        $("[data-test-id='phone'] input").setValue("+79650000000");
+        $("[data-test-id='agreement']").click();
+        $("[type='button']").click();
+        $("[data-test-id='order-success']")
+               .shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время"));
     }
+
 }
